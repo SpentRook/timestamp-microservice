c67d8b859f4aa0541b3930a5c2527db6596c64be
Richard Guevara <leonvilla6@outlook.com>
2022-12-02 14:56:43 -0500

    Endpoint to handle dates

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(cors("*"))
 
-app.use(express.static('public'));
+app.use(express.static('src/public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (_req, res) {
@@ -16,5 +16,29 @@ app.get('/', function (_req, res) {
 app.get('/api/hello', function (_req, res) {
     res.json({ greeting: 'hello API' });
   });
+
+app.get('/api/:date', (req, res)=> {
+    const reqDate = req.params.date
+    const dateFormatted = !isNaN(reqDate) ? +reqDate : reqDate
+    const date = new Date(dateFormatted)
+
+    if(date.toDateString() === 'Invalid Date') {
+        res.json({ error : "Invalid Date" })
+        return;
+    }
+
+    const unixDate = date.getTime();
+    const utcDate = date.toUTCString()
+    res.json({unix: unixDate, utc: utcDate})
+})
+
+app.get('/api/', (_req, res)=> {
+    const date = new Date()
+
+    const unixDate = date.getTime();
+    const utcDate = date.toUTCString()
+    
+    res.json({unix: unixDate, utc: utcDate})
+})
 
 module.exports = app
